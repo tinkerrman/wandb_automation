0cefa8117369f982afecfd32472caebeaedb7277
tinkerrman <tinkerrman@users.noreply.github.com>
2024-08-27 11:48:04 +0000

    deploy: 9bf7b9cb160ec25df96afdeb64292166b1be02c0

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const modelStatus = "";
-    const entityName = "";
-    const projectName = "";
-    const artifactCollectionName = "";
-    const modelId = "";
+    const modelStatus = "production";
+    const entityName = "wandb_Y72QKAKNEFI3G";
+    const projectName = "wandb-registry-model";
+    const artifactCollectionName = "ADAS handson models";
+    const modelId = "wandb-artifact://_id/QXJ0aWZhY3Q6MTE2NzM1MTk0OA==";
 
     const statusElement = document.getElementById('status');
     const announcementElement = document.getElementById('announcement');
